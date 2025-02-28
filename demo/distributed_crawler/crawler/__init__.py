06fe8fedd31fb739612821920299f9b90ab54eae
AliceZhaang <156692981+AliceZhaang@users.noreply.github.com>
2025-02-28 20:24:10 +0800

    Delete demo directory

--- demo/distributed_crawler/crawler/__init__.py
+++
@@ -1,4 +0,0 @@
-from .spiders.base_spider import BaseSpider
-from .middlewares.ua import RandomUserAgentMiddleware
-from .middlewares.proxy import ProxyMiddleware
-from .pipelines import MongoDBPipeline, ElasticsearchPipeline 
--- demo/distributed_crawler/crawler/spiders/worker.py
+++
@@ -1,7 +0,0 @@
-from .base_spider import BaseSpider
-
-class WorkerSpider(BaseSpider):
-    name = 'worker'
-    
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs) 
